a4d16d7397978a14c7326741ce0413d8699d81b0
Guzz <bragadagugudan@gmail.com>
2023-01-01 17:18:27 -0300

    changing crendentials+ more appropriate language

--- cms/config.php
+++ cms/config.php
@@ -3,17 +3,17 @@
 ini_set("display_errors", true);
 date_default_timezone_set("America/Sao_Paulo");
 define("DB_DSN", "mysql:host=localhost;dbname=cms");
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
+define("DB_USERNAME", "guzzy");
+define("DB_PASSWORD", "suika");
 define("CLASS_PATH", "classes");
 define("TEMPLATE_PATH", "templates");
 define("HOMEPAGE_NUM_ARTICLES", 5);
-define("ADMIN_USERNAME", "admin");
-define("ADMIN_PASSWORD", "suika");
+define("ADMIN_USERNAME", "guzz");
+define("ADMIN_PASSWORD", "reimu");
 require(CLASS_PATH . "/Article.php");
 
 function handleException($exception) {
-    echo "Sorry, a problem ocurred. Contact the dumbass developer";
+    echo "Sorry, a problem ocurred. Contact the silly developer";
     error_log($exception->getMessage());
 }
 
