5235be66bf5b58f5448a675f16b0f7c3a27911c5
Guzz <bragadagugudan@gmail.com>
2023-01-01 13:30:51 -0300

    footer include template added

---
+++ cms/templates/include/footer.php
@@ -0,0 +1,6 @@
+            <div id="footer">
+                Guzz's Blog &copy; 2023. All rights reserved. <a href="admin.php">Restricted Area!</a>
+            </div>
+        </div>
+    </body>
+</html>
