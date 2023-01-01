c2626348f59bb491cd1199832aaf8d6bf92223af
Guzz <bragadagugudan@gmail.com>
2023-01-01 15:50:01 -0300

    appropriate structure rework for styling

--- cms/templates/include/footer.php
+++ cms/templates/include/footer.php
@@ -1,5 +1,7 @@
-            <div id="footer">
-                Guzz's Blog &copy; 2023. All rights reserved. <a href="admin.php">Restricted Area!</a>
+            <div id="footer-container">
+                <div class="footer">                
+                    Guzz's Blog &copy; 2023. All rights reserved. <a href="admin.php">Restricted Area!</a>
+                </div>
             </div>
         </div>
     </body>
