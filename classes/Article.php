c7992e620128f0385a90610f175863d828d5e742
Guzz <bragadagugudan@gmail.com>
2023-01-03 20:57:06 -0300

    smol fix

--- classes/Article.php
+++ classes/Article.php
@@ -13,7 +13,7 @@ class Article
     public $summary = null; // brief description
     public $content = null; // the meat
 
-      /* le methods for manipulating data */
+    /* le methods for manipulating data */
 
     public function __construct($data = array())
     {
@@ -81,20 +81,19 @@ class Article
 
     public function insert()
     {
-        if (!is_null($this->id)) {
+        if (!is_null($this->id))
             trigger_error("Article::insert(): Attempt to insert an Article object that already has its ID property set (to this->id.", E_USER_ERROR);
 
-            $conn = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
-            $sql = "INSERT INTO articles (publicationDate, title, summary, content) VALUES (FROM_UNIXTIME(:publicationDate, :title, :summary, :content)";
-            $st = $conn->prepare($sql);
-            $st->bindValue(":publicationDate", $this->publicationDate, PDO::PARAM_INT);
-            $st->bindValue(":title", $this->title, PDO::PARAM_STR);
-            $st->bindValue(":summary", $this->summary, PDO::PARAM_STR);
-            $st->bindValue(":content", $this->content, PDO::PARAM_STR);
-            $st->execute();
-            $this->id = $conn->lastInsertId();
-            $conn = null;
-        }
+        $conn = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+        $sql = "INSERT INTO articles (publicationDate, title, summary, content) VALUES (FROM_UNIXTIME(:publicationDate), :title, :summary, :content)";
+        $st = $conn->prepare($sql);
+        $st->bindValue(":publicationDate", $this->publicationDate, PDO::PARAM_INT);
+        $st->bindValue(":title", $this->title, PDO::PARAM_STR);
+        $st->bindValue(":summary", $this->summary, PDO::PARAM_STR);
+        $st->bindValue(":content", $this->content, PDO::PARAM_STR);
+        $st->execute();
+        $this->id = $conn->lastInsertId();
+        $conn = null;
     }
     public function update()
     {
