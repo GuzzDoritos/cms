c378277f2eccabe6662e90ebe4234d6651a01a2f
Guzz <bragadagugudan@gmail.com>
2023-01-03 20:55:25 -0300

    restructure for styling

--- templates/include/footer.php
+++ templates/include/footer.php
@@ -1,8 +1,8 @@
+            <hr style="margin-top: 10px;">
             <div id="footer-container">
                 <div class="footer">                
-                    Guzz's Blog &copy; 2023. All rights reserved. <a href="admin.php">Restricted Area!</a>
+                    Guzz's Blog &copy; 2023. All rights reserved. <a href="admin.php" id="restricted">Restricted Area!</a>
                 </div>
             </div>
-        </div>
     </body>
 </html>
